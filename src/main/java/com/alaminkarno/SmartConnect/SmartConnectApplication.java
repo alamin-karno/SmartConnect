db6eec251b5152c67e6160469da3ccb69bf1f2c7
alamin-karno <amin15-1951@diu.edu.bd>
2024-08-14 21:44:02 +0600

    SCM: Initialization

---
+++ src/main/java/com/alaminkarno/SmartConnect/SmartConnectApplication.java
@@ -0,0 +1,13 @@
+package com.alaminkarno.SmartConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alaminkarno/SmartConnect/SmartConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaminkarno.SmartConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
